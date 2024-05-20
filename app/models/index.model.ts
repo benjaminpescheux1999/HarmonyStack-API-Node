9fd2dee02517f39bf2cd5ac3f64cc498f89b6ee3
benjaminpescheux1999 <47316003+benjaminpescheux1999@users.noreply.github.com>
2024-05-20 22:18:47 +0200

    fix init project

---
+++ app/models/index.model.ts
@@ -0,0 +1,12 @@
+// Importez votre modèle User depuis son fichier
+
+import RefreshToken, { IRefreshToken } from './refreshToken.model';
+import User, { IUser } from './user.model';
+import RatingStadium, { IRatingStadium } from './ratingStadium.model';
+
+// Exportez tous vos modèles
+export {
+    User, IUser,
+    RefreshToken, IRefreshToken,
+    RatingStadium, IRatingStadium
+};
